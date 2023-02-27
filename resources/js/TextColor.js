e2fbc615d976fed6de1cec1c8bbdd15f0dbab5d6
Bernhard Berger <b.berger@mmcagentur.at>
2023-02-27 17:22:48 +0100

    [FIX] fix #24 for new installations

--- resources/js/TextColor.js
+++ resources/js/TextColor.js
@@ -1,95 +1,76 @@
+import tinycolor from "tinycolor2";
 const { Mark } = Statamic.$bard.tiptap.core;
 
 export function isCloseToWhite(color) {
+    const processedColor = tinycolor(color);
 
-  if(color === 'transparent') {
-    return true;
-  }
+    if (!processedColor.isValid()) {
+        console.warn(`Invalid color format: ${color}`);
+        return false;
+    }
 
-  if(color === 'inherit') {
-    return false;
-  }
+    if (processedColor.getAlpha() === 0) {
+        return true;
+    }
 
-  let r, g, b;
-
-  if (color.startsWith('#') && color.length === 7) {
-    // HEX color - precise
-    r = parseInt(color.substring(1, 3), 16);
-    g = parseInt(color.substring(3, 5), 16);
-    b = parseInt(color.substring(5, 7), 16);
-
-    // 
-  } else if(color.startsWith('#') && color.length === 4) {
-    // HEX color - shorthand
-    r = parseInt(color.substring(1, 2), 16) * 17;
-    g = parseInt(color.substring(2, 3), 16) * 17;
-    b = parseInt(color.substring(3, 4), 16) * 17;
-  } else if (color.startsWith('rgb')) {
-    // RGB color - We're not bothering with alpha
-    const values = color.substring(4, color.length - 1).split(',');
-    r = parseInt(values[0]);
-    g = parseInt(values[1]);
-    b = parseInt(values[2]);
-  } else {
-    throw new Error(`Invalid color format: ${color}`);
-  }
-  return r >= 175 && g >= 175 && b >= 175;
+    const rgb = processedColor.toRgb();
+    return rgb.r >= 175 && rgb.g >= 175 && rgb.b >= 175;
 }
 
 export const TextColor = Mark.create ({
-  name: 'textColor',
+    name: 'textColor',
 
-  addAttributes() {
-    return {
-      key: {
-        default: '',
-        parseHTML: element => { element.querySelector('span.text-color')?.getAttribute('data-class') },
-      }
-    }
-  },
+    addAttributes() {
+        return {
+            key: {
+                default: '',
+                parseHTML: element => { element.querySelector('span.text-color')?.getAttribute('data-class') },
+            }
+        }
+    },
 
-  parseHtml() {
-    return [
-      {
-        tag: 'span.text-color'
-      }
-    ]
-  },
+    parseHtml() {
+        return [
+            {
+                tag: 'span.text-color'
+            }
+        ]
+    },
 
-  renderHTML({ HTMLAttributes }) {
-    let style = 'color: ';
+    renderHTML({ HTMLAttributes }) {
+        let style = 'color: ';
     style += HTMLAttributes.key
 
-    if (isCloseToWhite(HTMLAttributes.key)) {
-      style += '; text-shadow: 1px 2px #333';
-    }
-    
-    return [
-      'span',
-      {
-        ...HTMLAttributes,
-        class: 'text-color',
-        'data-class': HTMLAttributes.key,
-        'style': style
-      },
-      0
-    ]
-  },
+        if (isCloseToWhite(HTMLAttributes.key)) {
+            style += '; text-shadow: 1px 2px #333';
+        }
 
-  addCommands() {
-    return {
-      changeTextColor: attributes => ({ chain }) => {
-          if (attributes.key) {
-            return chain()
-              .setMark(this.name, attributes)
-              .run()
-          }
+        return [
+            'span',
+            {
+                ...HTMLAttributes,
+                class: 'text-color',
+                'data-class': HTMLAttributes.key,
+                'style': style
+            },
+            0
+        ]
+    },
 
-          return chain()
-            .unsetMark(this.name, { extendEmptyMarkRange: true })
-            .run()
-      },
+    addCommands() {
+        return {
+            changeTextColor: attributes => ({ chain }) => {
+                if (attributes.key) {
+                    return chain()
+                        .setMark(this.name, attributes)
+                        .run()
+                }
+
+                return chain()
+                    .unsetMark(this.name, { extendEmptyMarkRange: true })
+                    .run()
+            },
+        }
     }
-  }
 
 })
